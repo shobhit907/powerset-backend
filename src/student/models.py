d9d82be01aeb1c1e4b6c81e52f5cb01745ff4802
Shubham1427 <shubhamsingh14272000@gmail.com>
2021-02-21 12:26:31 +0530

    Max Length Added

--- src/student/models.py
+++ src/student/models.py
@@ -6,13 +6,13 @@ class Students (models.Model):
     #user (FK)
     #institute (FK)
     isVerified = models.BooleanField(default=False)
-    entryNumber = models.CharField()
-    branch = models.CharField()
-    degree = models.CharField()
-    motherName = models.CharField()
-    fatherName = models.CharField()
+    entryNumber = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    degree = models.CharField(max_length=50)
+    motherName = models.CharField(max_length=50)
+    fatherName = models.CharField(max_length=50)
     preferredProfile = models.CharField(blank=True)
-    category = models.CharField()
+    category = models.CharField(max_length=50)
     technicalSkills = models.CharField(blank=True)
     introduction = models.TextField(blank=True)
     careerPlans = mdoels.TextField(blank=True)
@@ -27,18 +27,18 @@ class SocialProfiles (models.Model):
 
 class Projects (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
-    domain = models.CharField()
+    title = models.CharField(max_length=50)
+    domain = models.CharField(max_length=50)
     startDate = models.DateField()
     endDate = models.DateField()
     description = models.TextField(blank=True)
 
 class Patents (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     Office = CountryField()
-    Number = models.CharField()
+    Number = models.CharField(max_length=50)
     Status = models.CharField(choices=[ ('P', 'Patent Pending'), ('I', 'Patent Issued')])
     filingDate = models.DateField()
 
@@ -49,17 +49,17 @@ class Resumes (models.Model):
 
 class AwardsAndRecognitions (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-    Issuer = models.CharField()
+    Issuer = models.CharField(max_length=50)
     issueDate = models.DateField()
     #associatedWith = list of all academic intitutes of student
 
 class WorkExperience (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    jobTitle = models.CharField()
-    company = models.CharField()
-    location = models.CharField()
+    jobTitle = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
     starDate = models.DateField()
     endDate = models.DateField()
     compensationMin = models.IntegerField()
@@ -68,29 +68,29 @@ class WorkExperience (models.Model):
 
 class Courses (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    courseCode = models.CharField()
-    title = models.CharField()
+    courseCode = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     gradeSecured = models.CharField(choices=['A', 'A-', 'B', 'B-', 'C', 'C-', 'F'])
 
 class Competitions (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
-    position = model.CharField()
+    title = models.CharField(max_length=50)
+    position = model.CharField(max_length=50)
     #associatedWith = list of all academic intitutes of student
     date = models.DateField()
     description = models.TextField(blank=True)
 
 class PositionsOfResponsibilities (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     fromDate = models.DateField()
     toDate = models.DateField()
-    organizationName = models.CharField()
+    organizationName = models.CharField(max_length=50)
 
 class Documents (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     link = models.URLField()
 
 class Semester (models.Model):
@@ -103,38 +103,38 @@ class Semester (models.Model):
 class Class (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
     number = models.IntegerField()
-    InstituteName = models.CharField()
+    InstituteName = models.CharField(max_length=50)
     fromDate = models.DateField()
     toDate = models.DateField()
     score = models.FloatField()
-    board = models.CharField()
-    stream = models.CharField()
+    board = models.CharField(max_length=50)
+    stream = models.CharField(max_length=50)
 
 class Certifications (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    name = models.CharField()
-    issuingAuthority = models.CharField()
+    name = models.CharField(max_length=50)
+    issuingAuthority = models.CharField(max_length=50)
     link = models.URLField()
     date = models.DateField()
-    licenseNumber = models.CharField()
+    licenseNumber = models.CharField(max_length=50)
     hasExpiry = models.BooleanField(default=False)
     description = models.TextField(blank=True)
 
 class ConferencesAndWorkshops (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-    organizor = models.charField()
+    organizor = models.CharField(max_length=50)
     address = models.TextField()
 
 class CommunicationLanguages (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    language = models.CharField()
+    language = models.CharField(max_length=50)
     proficiency = models.IntegerField()
 
 class TestScores (models.Model):
     student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     scoreFormat = models.CharField(choices=[('S', 'Score'), ('R', 'Rank'), ('P', 'Percentile')])
     score = models.FloatField()
