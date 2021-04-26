3848e6decf6572d6c1b903fb5ec83d2ea6cd8fb0
Shubham1427 <shubhamsingh14272000@gmail.com>
2021-04-26 20:06:47 +0530

    Placement Serializer Added in Student Read Serializer

--- src/serializers_common.py
+++ src/serializers_common.py
@@ -1,12 +1,13 @@
 from placement.serializers import InstituteSerializer, CoordinatorSerializer
 from accounts.serializers import *
 from student.models import Student
-from placement.serializers import JobProfileReadSerializer
+from placement.serializers import JobProfileReadSerializer, PlacementSerializer
 from placement.models import JobApplicant, Coordinator
 
 
 class StudentReadSerializer (serializers.ModelSerializer):
     institute = InstituteSerializer()
+    placement = PlacementSerializer()
     user = UserSerializer()
     coordinators = serializers.SerializerMethodField()
 
@@ -18,7 +19,7 @@ class StudentReadSerializer (serializers.ModelSerializer):
 
     class Meta:
         model = Student
-        fields = ('id', 'entry_number', 'is_verified', 'branch', 'institute', 'user', 'degree', 'mother_name',
+        fields = ('id', 'entry_number', 'is_verified', 'branch', 'institute', 'user', 'degree', 'mother_name', 'placement',
                   'father_name', 'preferred_profile', 'category', 'technical_skills', 'introduction', 'career_plans', 'coordinators', 'batch', 'cgpa', 'verification_message')
 
 
