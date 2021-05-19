330d9e5773c7080c8058a5c89041edb2fbae110a
Shubham1427 <shubhamsingh14272000@gmail.com>
2021-05-19 19:56:25 +0530

    Resume added to student read serializer

--- src/serializers_common.py
+++ src/serializers_common.py
@@ -1,8 +1,11 @@
+from rest_framework import serializers
 from placement.serializers import InstituteSerializer, CoordinatorSerializer
 from accounts.serializers import *
-from student.models import Student
+from student.models import Student, Resume
 from placement.serializers import JobProfileReadSerializer, PlacementSerializer
 from placement.models import JobApplicant, Coordinator
+from student.serializers import ResumeSerializer
+from django.shortcuts import get_object_or_404
 
 
 class StudentReadSerializer (serializers.ModelSerializer):
@@ -10,6 +13,7 @@ class StudentReadSerializer (serializers.ModelSerializer):
     placement = PlacementSerializer()
     user = UserSerializer()
     coordinators = serializers.SerializerMethodField()
+    primary_resume = serializers.SerializerMethodField()
 
     def get_coordinators(self, obj):
         coordinators_in = Coordinator.objects.filter(student=obj)
@@ -17,9 +21,13 @@ class StudentReadSerializer (serializers.ModelSerializer):
             coordinators_in, many=True).data
         return serialized_data
 
+    def get_primary_resume(self, obj):
+        pr = Resume.objects.filter(student=obj, is_latest=True).first()
+        return ResumeSerializer(pr).data
+
     class Meta:
         model = Student
-        fields = ('id', 'entry_number', 'is_verified', 'branch', 'institute', 'user', 'degree', 'mother_name', 'placement',
+        fields = ('id', 'entry_number', 'is_verified', 'branch', 'institute', 'user', 'degree', 'mother_name', 'placement', 'primary_resume',
                   'father_name', 'preferred_profile', 'category', 'technical_skills', 'introduction', 'career_plans', 'coordinators', 'batch', 'cgpa', 'verification_message')
 
 
